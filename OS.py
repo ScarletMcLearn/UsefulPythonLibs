3828005ff0e5600258c9266d818f7ffe45b5c94b
Unknown <ratul_shams@yahoo.com>
2016-12-11 01:58:10 +0600

    init

---
+++ OS.py
@@ -0,0 +1,12 @@
+import os
+
+pwd = os.getcwd()
+
+print(pwd)
+
+
+##os.mkdir("New Dixrectory")
+
+##os.rename("New Directory", "New Directory!!!")
+
+os.rmdir("New Directory!!!")
---
+++ Py2to3.py
@@ -0,0 +1,14 @@
+def greet(name):
+    print("Hello, {0}!".format(name))
+print("What's your name?")
+name = input()
+greet(name)
+
+
+
+##2to3 example.py
+
+##2to3 -w example.py
+
+
+##https://docs.python.org/2/library/2to3.html
